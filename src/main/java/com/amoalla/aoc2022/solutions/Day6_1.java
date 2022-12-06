b15d3026059bb51478fff410bdc73b3873ab7fa2
Ahmed Moalla <ahmed.moalla@gmail.com>
2022-12-06 17:11:27 +0100

    Day 6

---
+++ src/main/java/com/amoalla/aoc2022/solutions/Day6_1.java
@@ -0,0 +1,35 @@
+package com.amoalla.aoc2022.solutions;
+
+import com.amoalla.aoc2022.Day;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Day6_1 extends Day<Integer> {
+
+    private static final int START_OF_PACKET_SIZE = 4;
+
+    public Day6_1() {
+        super(6);
+    }
+
+    @Override
+    public Integer process() {
+        String buffer = allLines().get(0);
+        Deque<String> slidingWindow = new ArrayDeque<>();
+        char[] charArray = buffer.toCharArray();
+        int i;
+        for (i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            String str = Character.toString(c);
+            slidingWindow.addLast(str);
+            if (slidingWindow.size() == START_OF_PACKET_SIZE) {
+                if (slidingWindow.stream().distinct().count() == START_OF_PACKET_SIZE) {
+                    break;
+                }
+                slidingWindow.removeFirst();
+            }
+        }
+        return i + 1;
+    }
+}
